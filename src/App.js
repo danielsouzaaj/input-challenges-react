6a5461813551ac637e994596242b02a18c567bee
Daniel Souza <danielsouzaaj@hotmail.com>
2024-08-22 17:33:44 -0300

    updated app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
+import Project from './components/Project'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Input Challenges React</h1>
+      <section className='card-container'>
+        <Project title="Teste" tags={['html', 'css']} />
+      </section>
     </div>
   );
 }
