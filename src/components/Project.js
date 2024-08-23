16675cd98d13696f89d08945c0d1f6c7401aa8d2
Daniel Souza <danielsouzaaj@hotmail.com>
2024-08-23 04:46:50 -0300

    updated project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,18 @@
-import '../assets/project.css'
+import { Link } from 'react-router-dom'
+import '../assets/css/project.css'
+import icon from '../assets/images/arrow-up-right-from-square-solid.svg'
 
-function Project({title, tags}) {
+function Project({ title, link, preview }) {
     return (
         <article className="card">
+            <img src={preview} alt="preview" />
             <h2 className="card-title">{title}</h2>
-            <div className="tag-container">
-                {tags.map(tag => (<span>{tag}</span>))}
-            </div>
+            <Link to={link}>
+                <span className="link-container">
+                    <p>Demo</p>
+                    <img src={icon} alt="Demo link icon" />
+                </span>
+            </Link>
         </article>
     )
 }
